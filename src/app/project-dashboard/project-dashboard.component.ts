72af0822ab3273d3e5ef4071f54f14e1727b7f9a
Aakash Dasgupta <dasgupta_aakash@ymail.com>
2023-06-22 20:41:21 +0530

    added user and ticket lists in homepage

--- src/app/project-dashboard/project-dashboard.component.ts
+++ src/app/project-dashboard/project-dashboard.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-dashboard.component.css'],
 })
 export class ProjectDashboardComponent {
+  users:any = [
+    {
+      name: 'Aakash Dasgupta',
+      status: 'online'
+    },
+    {
+      name: 'Dhruv Agarwal',
+      status: 'offline'
+    },
+    {
+      name: 'Gunjan Tomar',
+      status: 'online'
+    },
+    {
+      name: 'Raghuraman',
+      status: 'offline'
+    },
+  ]
 
+  tickets: any = [
+    {
+      name: 'Write header code',
+      type: 'New Feature',
+      priority: 'Medium',
+      deadline: ''
+    },
+    {
+      name: 'Create Add Ticket Feature',
+      type: 'New Feature',
+      priority: 'Medium',
+      deadline: ''
+    },
+    {
+      name: 'Create Add Project Feature',
+      type: 'New Feature',
+      priority: 'Medium',
+      deadline: ''
+    },
+    {
+      name: 'Create add friends feature',
+      type: 'New Feature',
+      priority: 'Medium',
+      deadline: ''
+    }
+  ]
+
+  projects: any = [
+    {
+      name: '',
+      ticketPriorityCount: {
+        low: 0,
+        medium: 0,
+        high: 0
+      }
+    }
+  ]
 }
